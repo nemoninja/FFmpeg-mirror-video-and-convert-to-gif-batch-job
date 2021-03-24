e17acd12fbf2c1ef343f43c03841a0eb15148d11
nemoninja <35476293+nemoninja@users.noreply.github.com>
2021-03-25 01:15:32 +0800

    Added line to delete the temporary converted .mkv.

--- convert.bat
+++ convert.bat
@@ -1,7 +1,8 @@
 @echo off 
 for /F "tokens=*" %%a in (list.txt) do (
-ffmpeg -i %%a -c copy -an %%a.mkv
-ffmpeg -i %%a.mkv -filter_complex "[0:v]reverse,fifo[r];[0:v][r] concat=n=2:v=1 [v]" -map "[v]" %%a.gif
-echo "converted %%a"
+  ffmpeg -i %%a -c copy -an %%a.mkv
+  ffmpeg -i %%a.mkv -filter_complex "[0:v]reverse,fifo[r];[0:v][r] concat=n=2:v=1 [v]" -map "[v]" %%a.gif
+  del %%a.mkv
+  echo "converted %%a"
 )
 pause
