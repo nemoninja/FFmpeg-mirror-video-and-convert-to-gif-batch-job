03d4005d1f69c1647effb43905436742ad9faeaf
nemoninja <35476293+nemoninja@users.noreply.github.com>
2021-03-25 00:23:51 +0800

    Added convert.bat and list.txt .

---
+++ convert.bat
@@ -0,0 +1,7 @@
+@echo off 
+for /F "tokens=*" %%a in (list.txt) do (
+ffmpeg -i %%a -c copy -an %%a.mkv
+ffmpeg -i %%a.mkv -filter_complex "[0:v]reverse,fifo[r];[0:v][r] concat=n=2:v=1 [v]" -map "[v]" %%a.gif
+echo "converted %%a"
+)
+pause
